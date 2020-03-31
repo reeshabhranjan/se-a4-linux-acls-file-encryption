48fe0a49dde2e77d02ca7639686768b3935074b7
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-03-31 20:31:29 +0530

    corrected the key length (bits instead of bytes)

--- encrypt.c
+++ encrypt.c
@@ -10,8 +10,8 @@
 #include "acl.h"
 
 // common constants
-const int KEY_SIZE = 256;
-const int IV_SIZE = 128;
+const int KEY_SIZE_BITS = 256; // bits
+const int IV_SIZE_BITS = 128; // bits
 
 // functions for encryption-decryption and sign-verify
 
@@ -36,8 +36,13 @@ void generate_key_iv(char** key, char** iv)
 {
     char* passphrase = get_password(); // TODO what if there is no password? (NULL or something else?)
     char* salt = get_username();
-    char key_iv[50];
-    PKCS5_PBKDF2_HMAC_SHA1(passphrase, strlen(passphrase), salt, strlen(salt), 1024, KEY_SIZE + IV_SIZE, key_iv);
-    *key = substring(key_iv, 0, KEY_SIZE);
-    *iv = substring(key_iv, KEY_SIZE, KEY_SIZE + IV_SIZE);
+    char key_iv[KEY_SIZE_BITS / 8 + IV_SIZE_BITS / 8 + 10]; // 10 as a buffer if anything goes unexpected
+    PKCS5_PBKDF2_HMAC_SHA1(passphrase, strlen(passphrase), salt, strlen(salt), 1024, KEY_SIZE_BITS / 8 + IV_SIZE_BITS / 8, key_iv);
+    *key = substring(key_iv, 0, KEY_SIZE_BITS / 8);
+    *iv = substring(key_iv, KEY_SIZE_BITS / 8, KEY_SIZE_BITS / 8 + IV_SIZE_BITS / 8);
+}
+
+char* encrypt_string(char* plaintext, char* key, char* iv)
+{
+
 }
