53dd5c136465c28c462afa44c5c45965e83ff5db
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2020-04-25 20:51:11 +0530

    Revert code back to write, read syscalls

--- utils.c
+++ utils.c
@@ -41,22 +41,22 @@ void write_to_file(char* filepath, char* buffer, int overwrite)
 
 void write_to_file_with_len(char* filepath, char* buffer, int len, int overwrite)
 {
-    // int fd = -1;
-    // if (overwrite)
-    // {
-    //     fd = open(filepath, O_WRONLY | O_CREAT | O_TRUNC);
-    // }
-    // else
-    // {
-    //     fd = open(filepath, O_WRONLY | O_APPEND | O_CREAT);
-    // }
+    int fd = -1;
+    if (overwrite)
+    {
+        fd = open(filepath, O_WRONLY | O_CREAT | O_TRUNC);
+    }
+    else
+    {
+        fd = open(filepath, O_WRONLY | O_APPEND | O_CREAT);
+    }
     
-    // write(fd, buffer, len);
-    // close(fd);
+    write(fd, buffer, len);
+    close(fd);
 
-    FILE* fp = fopen(filepath, "w");
-    fwrite(buffer, 1, len, fp);
-    fclose(fp);
+    // FILE* fp = fopen(filepath, "w");
+    // fwrite(buffer, 1, len, fp);
+    // fclose(fp);
 }
 
 void create_file(char* filepath, int owner_id, int group_id, int permissions)
